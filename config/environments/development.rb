596f569f91c9151479872926248986870408f607
ABHISHEK VERMA <abhishekverma@ABHISHEKs-Air.bbrouter>
2024-08-11 18:50:57 +0530

    chore: Update Gemfile.lock and environment configuration for dotenv-rails gem and SendGrid settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,8 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -74,5 +75,9 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.action_mailer.default_url_options = { host: 'http://65.0.138.113/' }
+  # config.action_mailer.default_url_options = { host: 'http://65.0.138.113/' }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
