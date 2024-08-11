59c15af381cee1200b881b7d828cd27a4a97d5bf
ABHISHEK VERMA <abhishekverma@ABHISHEKs-Air.bbrouter>
2024-08-11 16:44:01 +0530

    style: Add thank-you page and route for successful contact form submission

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,14 +1,5 @@
 class ContactsController < ApplicationController
-  before_action :set_contact, only: %i[ show edit update destroy ]
-
-  # GET /contacts or /contacts.json
-  def index
-    @contacts = Contact.all
-  end
-
-  # GET /contacts/1 or /contacts/1.json
-  def show
-  end
+  before_action :set_contact, only: %i[edit]
 
   # GET /contacts/new
   def new
@@ -21,41 +12,19 @@ class ContactsController < ApplicationController
 
   # POST /contacts or /contacts.json
   def create
-    @contact = Contact.new(contact_params)
+    contact = Contact.new(contact_params)
 
-    respond_to do |format|
-      if @contact.save
-        format.html { redirect_to contact_url(@contact), notice: "Contact was successfully created." }
-        format.json { render :show, status: :created, location: @contact }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
-      end
+    if contact.save
+      flash[:notice] = "Contact was successfully created."
+      redirect_to thank_you_path()
+    else
+      flash[:error] = "Contact was not created."
     end
   end
 
-  # PATCH/PUT /contacts/1 or /contacts/1.json
-  def update
-    respond_to do |format|
-      if @contact.update(contact_params)
-        format.html { redirect_to contact_url(@contact), notice: "Contact was successfully updated." }
-        format.json { render :show, status: :ok, location: @contact }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
-      end
-    end
+  def thanks
   end
 
-  # DELETE /contacts/1 or /contacts/1.json
-  def destroy
-    @contact.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to contacts_url, notice: "Contact was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
